d7721347ecfe84ef866fc4da75be03115ff48154
Fadel Nararia Rahman <47631524+fadelrahman31@users.noreply.github.com>
2019-11-16 19:39:45 +0700

    Update for nav bar

--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -6,10 +6,19 @@
             absolute
             temporary            
         >
-            <v-btn color="blue lighten-3" fab dark>
-                <v-icon>mdi-account-circle</v-icon>
-            </v-btn>
             <v-divider></v-divider>
+
+            <v-card
+                class="ma-3 pa-1"
+                color="teal lighten-4"
+                raised
+                tile
+            >
+                <v-card-title class= "headline font-weight-medium">
+                    Hi, User!
+                </v-card-title>
+                <v-icon>mdi-account-circle</v-icon>
+            </v-card>
             <v-list nav dense>
                 <v-list-item
                     v-for= "item in items"
@@ -28,13 +37,17 @@
             absolute
             color="teal lighten-2"
             elevate-on-scroll
+            dark
         >
             <v-app-bar-nav-icon
                 @click.stop="drawer = !drawer"
             ></v-app-bar-nav-icon><v-spacer></v-spacer>
-            <v-toolbar-title>{{ appTitle }}</v-toolbar-title>                
+            <v-toolbar-title class= "font-weight-bold">{{ appTitle }}</v-toolbar-title>                
             <v-spacer flat></v-spacer>
-            <v-btn flat>sign in</v-btn>
+            <v-btn 
+                flat
+                color="teal darken-3"
+            >sign in</v-btn>
         </v-app-bar>
     </span>
 </template>
