301d26ba1c4101feb3180d976c10c016932d1e2e
Fadel Nararia Rahman <47631524+fadelrahman31@users.noreply.github.com>
2019-11-17 17:06:08 +0700

    update for landing page UI

--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -5,51 +5,57 @@
         >
             <v-col  
                 cols= "12"
+                md= "2"
+            >
+
+            </v-col>
+            <v-col  
+                cols= "12"
                 md= "9"
             >
                 <v-row 
                     v-for= "(item,i) in kostList"
                     :key= "i"
                     justify="space-between" 
-
+                    style= "flex-wrap: nowrap;"
                 >   
-                    <v-card
-                        class="mx-auto ma-3"
-                        max-width="1000"
-                        color="teal lighten-4"
-                        raised
-                        tile                
+                    <v-col
+                        cols= "7"
+                        class= "flex-grow-1 flex-shrink-0"
                     >
-                        <div class="d-flex flex-no-wrap justify-space-between">
-                            <div>
-                                <v-card-title
-                                    class= "headline"
-                                    v-text= "item.nama"
-                                ></v-card-title>
+                        <v-card
+                            class="mx-auto ma-3"
+                            max-width="1000"
+                            max-height="600"
+                            color="teal lighten-4"
+                            raised
+                            tile                
+                        >
+                            <div class="d-flex flex-no-wrap justify-space-between">
+                                <div>
+                                    <v-card-title
+                                        class= "headline"
+                                        v-text= "item.nama"
+                                    ></v-card-title>
 
-                                <v-card-subtitle v-text= "item.harga"></v-card-subtitle>
+                                    <v-card-subtitle v-text= "item.harga"></v-card-subtitle>
 
-                                <v-card-actions>
-                                    <v-btn text>Details</v-btn>
-                                </v-card-actions>
+                                    <v-card-actions>
+                                        <v-btn text>Details</v-btn>
+                                    </v-card-actions>
+                                </div>
+
+                                <v-avatar
+                                    class= "ma-3"    
+                                    size= "125"
+                                    tile
+                                >
+                                    <v-img :src= "item.gambar"></v-img>
+                                </v-avatar>
                             </div>
-
-                            <v-avatar
-                                class= "ma-3"    
-                                size= "125"
-                                tile
-                            >
-                                <v-img :src= "item.gambar"></v-img>
-                            </v-avatar>
-                        </div>
-                    </v-card>
+                        </v-card>
+                    </v-col>    
                 </v-row>
-            </v-col>
-            <v-col  
-                cols= "12"
-                md= "3"
-            >
-            
             </v-col>
         </v-row>
     </v-container>
